5d9e2d36fd3dedf73ffda14193932a78819b678a
Pujan Budhathoki <pujan.budhathoki@hotmail.com>
2020-07-09 18:22:37 -0500

    corrected variable's name in college's MODEL

--- vastika.college-info/model/College.java
+++ vastika.college-info/model/College.java
@@ -4,86 +4,77 @@ import java.util.Date;
 import java.util.List;
 
 public class College {
-    private Long id;
-    private String name;
-    private String phone;
-    private String deanName;
-    private Date estd;
+	private Long id;
+	private String name;
+	private String phone;
+	private String deanName;
+	private Date estd;
 
-    private Address address;
-    private List<Course> courses;
+	private Address address;
+	private List<Course> courses;
 
-    public College() {
-    }
+	public College() {
+	}
 
-    public College(Long id, String name, String phonr, String deanName, Date estd) {
-        this.id = id;
-        this.name = name;
-        this.phone = phonr;
-        this.deanName = deanName;
-        this.estd = estd;
-    }
+	public College(Long id, String name, String phone, String deanName, Date estd) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.deanName = deanName;
+		this.estd = estd;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
-    public List<Course> getCourses() {
-        return courses;
-    }
+	public List<Course> getCourses() {
+		return courses;
+	}
 
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public String getDeanName() {
+		return deanName;
+	}
 
+	public void setDeanName(String deanName) {
+		this.deanName = deanName;
+	}
 
-    
+	public Date getEstd() {
+		return estd;
+	}
 
-    public String getDeanName() {
-        return deanName;
-    }
+	public void setEstd(Date estd) {
+		this.estd = estd;
+	}
 
-    public void setDeanName(String deanName) {
-        this.deanName = deanName;
-    }
-
-    public Date getEstd() {
-        return estd;
-    }
-
-    public void setEstd(Date estd) {
-        this.estd = estd;
-    }
-
-    @Override
-    public String toString() {
-        return "College{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phonr='" + phone + '\'' +
-                ", deanName='" + deanName + '\'' +
-                ", estd=" + estd +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "College{" + "id=" + id + ", name='" + name + '\'' + ", phone='" + phone + '\'' + ", deanName='"
+				+ deanName + '\'' + ", estd=" + estd + '}';
+	}
 }
